8b65b5f8535c0b6b258cb3edcc10092aecad60b6
DylanKojiCheslin <lucidsandman@gmail.com>
2015-06-17 10:43:47 -0600

    changed from insert to updatesome,some styling

--- quiz-app/client/Templates/card/card.js
+++ quiz-app/client/Templates/card/card.js
@@ -1,5 +1,40 @@
 Template.card.events({
   "click .effect__click": function(event, template){
     $(event.currentTarget).toggleClass("flipped");
+  },
+  "click .startEditingCard": function (event, template){
+    $(event.currentTarget).parent().toggle(false);
+    $(event.currentTarget).parent().parent().children('.cardUpdate').toggle(true);
+    //toggle other sibling submitEditCart
+    //$(".submitEditCard").parent().children('.addCard')
+  },
+  "click .submitEditCard": function (event, template){
+    $(event.currentTarget).parent().toggle(false);
+    $(event.currentTarget).parent().parent().children('.cardOutline').toggle(true);
   }
 });
+//
+// Template.card.events({
+//   "click .effect__click .editCard": function(){
+//     Session.set("editedCardId", this._id);
+//   }
+// });
+//
+// Template.card.helpers({
+//   editedCardDoc: function () {
+//     return People.findOne(Session.get("editedCardId"));
+//   },
+//   isEditedCard: function () {
+//     return Session.equals("editedCardId", this._id);
+//   },
+//   formType: function () {
+//     if (Session.get("editedCardId")) {
+//       return "update";
+//     } else {
+//       return "disabled";
+//     }
+//   },
+//   disableButtons: function () {
+//     return !Session.get("editedCardId");
+//   }
+// });
